22a42bae92cd63f86b1329f2ead29faf8a066d2f
Abdulfattah04 <umarabdulfatah04@gmail.com>
2025-07-23 16:01:34 +0100

    Files debuging

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,5 @@
 import { Webhook } from "svix";
-import User from "../models/User.js";2
+import User from "../models/User.js";
 
 // API Controller function to manage clerk User with Database
 
@@ -10,7 +10,7 @@ export const clerkWebhooks = async (req, res)=> {
        await whook.verify(JSON.stringify(req.body), {
         "svix-id": req.headers["svix-id"],
         "svix-timestamp": req.headers["svix-timestamp"],
-        "svix-signature": req.headers["svix-signature"],
+        "svix-signature": req.headers["svix-signature"]
        })
 
        const {data, type} = req.body
